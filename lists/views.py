99bbdca6783562719fb73cac435f275f74819af4
czl <995454025@qq.com>
2022-04-19 09:16:08 +0800

    Basic view now return minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # home_page = None
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 
 # Create your views here.
