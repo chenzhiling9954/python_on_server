bbdb2efec8bbd4e9c7f11b99c45d221e53d3add8
czl <995454025@qq.com>
2022-05-10 09:34:11 +0800

    after reconstitution

--- functional_test.py
+++ functional_test.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -36,14 +41,23 @@ class NewVisitorTest(unittest.TestCase):
         # "1:Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock features to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+
+        # table = self.browser.find_element(By.ID, 'id_list_table')
+        # rows = table.find_element_by_tag_name('tr')
+        # self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        # self.assertIn(
+        #     '2: Use peacock features to make a fly',
+        #     [row.text for row in rows]
+        # )
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
         #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
