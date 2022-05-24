84e913c83fa356ca13bf4501ce487b224700d2c5
czl <995454025@qq.com>
2022-05-24 13:46:21 +0800

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,14 @@ from lists.models import Item
 
 # home_page = None
 def home_page(request):
-    if request.method == 'POST':
+    if request.method=='POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items':items})
+    return render(request, 'list.html', {'items': items})
 
     # else:
     #     new_item_text = ''
