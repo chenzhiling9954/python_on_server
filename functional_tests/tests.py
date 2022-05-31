fa769293b133b46dc21c492e1f5cd5c18ed8fcb7
czl <995454025@qq.com>
2022-05-31 13:41:08 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,7 +32,6 @@ class NewVisitorTest(LiveServerTestCase):
 
 
     def test_can_start_a_list_for_one_user(self):
-    # def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check out its homepage
         #self.browser.get('http://localhost:8000')
         self.browser.get(self.live_server_url)
